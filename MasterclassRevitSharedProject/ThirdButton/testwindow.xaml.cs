311b991060bfa7a62da26152bcd7b8c983912551
Chris Bowker <chris@valorVDC.com>
2023-05-11 18:34:37 -0400

    adding SharedProjectVideo again

---
+++ MasterclassRevitSharedProject/ThirdButton/testwindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Masterclass.Revit.ThirdButton
+{
+    public partial class testwindow : Window
+    {
+        public testwindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
